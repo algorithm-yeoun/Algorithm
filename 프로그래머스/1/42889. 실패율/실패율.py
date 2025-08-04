f222d37166d5e32646776a39912d63f205a51579
JunHyeok Heo <devyeoun@gmail.com>
2025-08-04 14:51:41 +0900

    [level 1] Title: 실패율, Time: 2700.84 ms, Memory: 13.9 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -1,14 +1,11 @@
 def solution(N, stages):
     answer = {i+1:0 for i in range(N)}
-    
     right=len(stages)
-    left=0
     
     for i in range(1,N+1):
         left=len([s for s in stages if s==i])
         fail=(left/right) if left else 0
         answer[i]=fail
-        
         right-=left
     
     answer = [k for k,v in sorted(answer.items(), key=lambda x: (-x[1], int(x[0])))]
