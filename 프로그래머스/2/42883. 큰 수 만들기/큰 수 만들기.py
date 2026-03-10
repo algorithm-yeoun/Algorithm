fac8653caa39d4005ba3c5f785a39cc29fb05f64
JunHyeok Heo <devyeoun@gmail.com>
2026-03-10 15:23:00 +0900

    [level 2] Title: 큰 수 만들기, Time: 92.97 ms, Memory: 13.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -0,0 +1,10 @@
+def solution(number, k):
+    stack = []
+    
+    for i in number:
+        while stack and k>0 and stack[-1]<i:
+            stack.pop()
+            k-=1
+        stack.append(i)
+            
+    return ''.join(stack[:len(number)-k])
